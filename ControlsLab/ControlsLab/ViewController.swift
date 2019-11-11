08a52ca78d727aff917971d11c81460c84f1c481
Juan Ceballos <juanceballos@Juans-MacBook-Pro.local>
2019-11-11 17:51:37 -0500

    Everything except face cards

--- ControlsLab/ControlsLab/ViewController.swift
+++ ControlsLab/ControlsLab/ViewController.swift
@@ -2,9 +2,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var stepperControl: UIStepper!
+    
+    @IBOutlet var collectionValue: [UILabel]!
+    @IBOutlet var collectionSuit: [UILabel]!
+    
+    var currentSegmentIndex: Int = 0    {
+        didSet  {
+            switch segmentedControl.selectedSegmentIndex {
+            case 0:
+                for suitImage in collectionSuit {
+                    suitImage.text = "♦️"}
+            case 1:
+                for suitImage in collectionSuit {
+                    suitImage.text = "♥️"}
+            case 2:
+                for suitImage in collectionSuit {
+                    suitImage.text = "♠️"}
+            case 3:
+                for suitImage in collectionSuit {
+                    suitImage.text = "♣️"}
+            default:
+                print("Error")
+            }
+        }
+    }
+    
+    var value: Int = 7  {
+        didSet  {
+            for value in collectionValue    {
+                value.text = "\(Int(stepperControl.value))"
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configureStepper()
     }
+    
+    func configureStepper() {
+        stepperControl.minimumValue = 2
+        stepperControl.maximumValue = 10
+        stepperControl.stepValue = 1
+        
+        stepperControl.value = 7
+    }
+    
+    @IBAction func SegmentedAction(_ sender: UISegmentedControl) {
+        currentSegmentIndex = sender.selectedSegmentIndex
+    }
+    
+    @IBAction func stepperAction(_ sender: UIStepper) {
+        value = Int(sender.value)
+    }
+    
 }
 
